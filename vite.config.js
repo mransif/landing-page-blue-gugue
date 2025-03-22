68395aaad3cefbabd75f57dc0c6b0f3c039afc93
Kenz8489 <stark858981@gmail.com>
2025-03-23 00:31:07 +0530

    updated gh deploy

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mransif/',
+  base: '/landing-page-blue-gugue/',
   build: {
     chunkSizeWarningLimit: 4000,  // Increase the limit (default is 500)
   },
